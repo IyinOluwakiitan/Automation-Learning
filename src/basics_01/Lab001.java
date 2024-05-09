cefb35fb10fe099606abac926da2b8627e6a276e
Omotolani <stilltolani@yahoo.com>
2024-05-08 22:04:36 -0600

    my new comment

--- src/basics_01/Lab001.java
+++ src/basics_01/Lab001.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Omotolani");
+        System.out.println("Omotolani");
     }
 }
