afd32c26f5100191a5fad94c4d137622d2e4a757
Omotolani <stilltolani@yahoo.com>
2024-05-08 22:06:06 -0600

    testing message

--- src/basics_01/Lab001.java
+++ src/basics_01/Lab001.java
@@ -6,5 +6,6 @@ public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Omotolani");
         System.out.println("Omotolani");
+        System.out.println("Omotolani");
     }
 }
