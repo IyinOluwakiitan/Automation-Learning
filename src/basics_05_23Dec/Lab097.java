40a124c5839be0b03bae2b8b1561a75ecc8b8171
Omotolani <stilltolani@yahoo.com>
2024-05-29 21:05:07 -0600

    personal attempt

---
+++ src/basics_05_23Dec/Lab097.java
@@ -0,0 +1,21 @@
+package src.basics_05_23Dec;
+
+public class Lab097 {
+    public static void main(String[] args) {
+//        int i = 10;
+//        while (i > 10) {
+//            System.out.println(i);
+//            i++;
+
+            int i = 10;                 //Int
+            do {
+                System.out.println(i);  //Body
+                i++;                    //Increment
+            } while (i < 10);            //Condition
+        }
+    }
+
+
+
+//major difference btw while & do while loop
+//is that if you want to execute the block at least once then use do-while statement
---
+++ src/basics_05_23Dec/Lab097b.java
@@ -0,0 +1,16 @@
+package src.basics_05_23Dec;
+
+public class Lab097b {
+    public static void main(String[] args) {
+        //Prime Number checker - Is No 45 a prime or not
+        //Factorial Example
+        //Leap Year checker  - 2000? -> is it a leap year
+        // True or False
+        int i = 10; //int
+        do {
+            System.out.println(i); //Body
+            i++; //Increment
+        } while (i < 10); //Condition
+        }
+    }
+
